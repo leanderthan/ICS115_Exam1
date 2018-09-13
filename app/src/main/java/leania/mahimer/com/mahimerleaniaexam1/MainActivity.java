a5d31f37dc1f27ab0931b7c5e1fa3722f3eae68a
jrossdelacruz <2015081632@ust-ics.mygbiz.com>
2018-09-13 14:59:30 +0800

    MAHIMER, LEANIA MAHIMER A. 4IT-E

---
+++ app/src/main/java/leania/mahimer/com/mahimerleaniaexam1/MainActivity.java
@@ -0,0 +1,14 @@
+package leania.mahimer.com.mahimerleaniaexam1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //comment
+}
